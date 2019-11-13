ae522307a062a9f7d4f28c0aca760cf31e6e5cc9
tcc62 <tcc62@bath.ac.uk>
2019-11-13 18:13:47 +0000

    Tile size now driven by maxe.txt
    Also updated the design of the wall tile and put it in code_resources

--- Code/mazebuilder.py
+++ Code/mazebuilder.py
@@ -1,6 +1,12 @@
 import random, math, pygame
 from pygame.locals import *
 
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+WINDOW_NAME = "Jeff's Lair"
+
+
 #print_text function
 def print_text(font, x, y, text, color=(255,255,255)):
     imgText = font.render(text, True, color)
@@ -8,15 +14,12 @@ def print_text(font, x, y, text, color=(255,255,255)):
 
 #main program begins
 pygame.init()
-screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Jeff's Lair")
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption(WINDOW_NAME)
 font = pygame.font.Font(None, 18)
 
-#load bitmaps
-tile = pygame.image.load("../Code_resources/tile.png").convert()
-
 # read file
-myfile = open("../Code_resources/maze.txt", "r")
+myfile = open("../code_resources/maze.txt", "r")
 
 # read the texgt file into list of lines
 mazelines = myfile.read().splitlines()
@@ -24,24 +27,34 @@ myfile.close()
 
 
 # getting length of list
-linelength = len(mazelines)
+num_lines = len(mazelines)
+
+# assume all line lengths are the same as top line
+maze_pattern_width = len(mazelines[0])
+maze_pattern_height = num_lines
+
+# work out tile width and height
+tile_width = SCREEN_WIDTH // maze_pattern_width
+tile_height = SCREEN_HEIGHT // maze_pattern_height
+
+#load bitmaps
+tile = pygame.image.load("../code_resources/plain_tile.png").convert()
+tile = pygame.transform.scale(tile, (tile_width, tile_height))
+
 current_x = 0
 current_y = 0
 
 # Iterating the index
 # same as 'for i in range(len(list))'
-for i in range(linelength):
+for i in range(num_lines):
     len_j = len(mazelines[i])
     this_line = mazelines[i]
     for j in range(len_j):
-        if this_line[j] == "0":
-            print_text(font, current_x, current_y, " ")
-        else:
-            print_text(font, current_x, current_y, "Wall")
+        if this_line[j] == "1":
             screen.blit(tile, (current_x, current_y))
 
-        current_x += 100
-    current_y += 100
+        current_x += tile_width
+    current_y += tile_height
     current_x = 0
 # # set y position to zero
 # posy = 0
