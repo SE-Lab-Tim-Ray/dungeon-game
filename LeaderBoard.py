c9871bb9ad9044fa0b18aa6aeb3ff2d08b507235
PinChouKu <37031468+PinChouKu@users.noreply.github.com>
2019-12-02 13:27:32 +0000

    Update LeaderBoard.py

--- LeaderBoard.py
+++ LeaderBoard.py
@@ -4,10 +4,10 @@ class LeaderBoard:
         self.leader_board = open(LEADER_BOARD_PATH, 'r')
         self.leader_board.close()
     def board_input_result(nickname='NoName', score=0):
-        leader_board_name = str(nickname).rjust(20)
+        leader_board_name = str(nickname).ljust(20)
         leader_board_score = str(score).rjust(10)
-        #the content we for one line we put into the screen is leader_board_result
-        leader_board_result = leader_board_name+'   '+leader_board_score+'\n'
+        #the content for one line we put into the screen is leader_board_result
+        leader_board_result = leader_board_name+leader_board_score+'\n'
         #open the file and write the result into txt file
         with open(LEADER_BOARD_PATH, 'r') as in_file:
             contents = in_file.readlines()
@@ -15,11 +15,18 @@ class LeaderBoard:
             if not contents:
                 out_file.write(leader_board_result)
             elif contents:
+                flag = True
                 for line in contents:
-                    if int(line[22:32]) <= score:
+                    if int(line[20:30].strip()) <= score and flag:
                         line = leader_board_result + line
+                        flag = False
+                    elif int(line[20:30].strip()) > score and flag:
+                        line = line + leader_board_result
+                        print(int(line[20:30].strip()))
+                        flag = False
                     out_file.write(line)
-                contents = out_file.readlines()
+        with open(LEADER_BOARD_PATH, 'r') as final_file:
+            contents = final_file.readlines()
         #the new list of leaderboard
         return contents
 
