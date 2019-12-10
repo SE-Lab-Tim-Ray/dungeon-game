0238a53d6161560b71b2e2795d6410d4983ab970
PinChouKu <37031468+PinChouKu@users.noreply.github.com>
2019-12-10 22:23:15 +0000

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='JeffisLiar',
     packages = ['JeffisLiar'],
-    version='1.4',
+    version='1.6',
     license='MIT',
     description='JeffisLiar. Have fun with our game and help Jeff get out of the dungeon!',
     author='Binzhou Gu',
     author_email='bg548@bath.ac.uk',
     url='https://github.com/SE-Lab-Tim-Ray/dungeon-game/tree/installation',
-    download_url='https://github.com/SE-Lab-Tim-Ray/dungeon-game/archive/V1.4.tar.gz',
+    download_url='https://github.com/SE-Lab-Tim-Ray/dungeon-game/archive/V1.6.tar.gz',
     keywords=['DungeonGame', 'Joyful'],
     install_requires=[
         'pygame',
