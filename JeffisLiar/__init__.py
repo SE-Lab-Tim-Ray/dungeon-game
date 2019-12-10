18323da6313c0f673fefd20cfea4572081174f24
PinChouKu <37031468+PinChouKu@users.noreply.github.com>
2019-12-10 20:51:10 +0000

    Delete __init__.py

--- JeffisLiar/__init__.py
+++
@@ -1,8 +0,0 @@
-from Hero import *
-from GameMap import *
-from Startbox import *
-from main import *
-
-if __name__ == "__main__":
-
-    Game(GAME_TITLE, SCREEN_SIZE[0], SCREEN_SIZE[1], nickname = startbox())
