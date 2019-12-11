e7301fc49984dd06e32943700dc235682ff3b8d4
PinChouKu <37031468+PinChouKu@users.noreply.github.com>
2019-12-11 01:36:53 +0000

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='JeffisLiar',
     packages = ['JeffisLiar'],
-    version='1.7',
+    version='1.8',
     license='MIT',
     description='JeffisLiar. Have fun with our game and help Jeff get out of the dungeon!',
     author='Binzhou Gu',
     author_email='bg548@bath.ac.uk',
     url='https://github.com/SE-Lab-Tim-Ray/dungeon-game/tree/installation',
-    download_url='https://github.com/SE-Lab-Tim-Ray/dungeon-game/archive/V1.7.tar.gz',
+    download_url='https://github.com/SE-Lab-Tim-Ray/dungeon-game/archive/V1.8.tar.gz',
     keywords=['DungeonGame', 'Joyful'],
     install_requires=[
         'pygame',
@@ -21,6 +21,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+    ],
+    include_package_data=True,
+    data_files=[
+        ('data', ['JeffisLiar/data/1_char.png', 'JeffisLiar/data/1_lose.png', 'JeffisLiar/data/1_maze.png',
+                  'JeffisLiar/data/1_maze.txt', 'JeffisLiar/data/1_win.png', 'JeffisLiar/data/rat.png'])
     ],
 )
